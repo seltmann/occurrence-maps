b8b1396f2c1925bd0d550dfebbcb6864cc564e8d
seltmann <enicospilus@gmail.com>
2020-11-30 16:52:58 -0800

    update todo

--- filter_polygon.R
+++ filter_polygon.R
@@ -1,8 +1,11 @@
 # Katja Seltmann, 2020
-# Script to map occurrence records of species (lat/long) that occur only within the boundries of Coal Oil Point Reserve (shp file)
+# Script to map occurrence records of species (lat/long) that occur only within the boundaries of Coal Oil Point Reserve (shp file)
 # COPR occurrence records in tab-delimited format: https://ucsb.box.com/s/7xp88xhg1xn7decsv0t3ll8du653deub
 # COPR boundary files: https://ucsb.box.com/s/nd1s0e3ted8zsu0ir4wbxu7qpe94ht8o
 
+#TODO:
+#draw boundary of copr on map
+#Trim specimen_data based on copr boundary creating new file that only contains specimens whose coordinates are within that boundary
 
 setwd("~/Box Sync/CCBER_Projects/Symbiota_UCSB_Data_Portal")
 
@@ -34,15 +37,15 @@ head(specimen_data_less)
 #print dimensions of new dataset
 dim(specimen_data_less)
 
-#read boundry from shp file
+#read boundary from shp file
 copr_boundary_2020 <- st_read("COPR/COPR_Boundary_2010/COPR_boundary2010.shp")
-
-#reset with new plot when needed
-plot.new()
 
 #graph boundary and points
 ggplot(data = copr_boundary_2020) + 
   geom_point(data = specimen_data_less, aes(x = decimalLongitude, y = decimalLatitude), shape = 1)
+
+
+
 
      
 
